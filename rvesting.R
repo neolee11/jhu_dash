c99e43913b240ebe40d24857eefd7b45d8ac8e87
Aaron Fisher <fisher@jhu.edu>
2015-09-21 17:46:00 -0400

    cleaner rvest code

--- rvesting.R
+++ rvesting.R
@@ -56,21 +56,61 @@ data.frame(dates,isFood,isExercise)
 names(tab)
 
 
+processTable <- function(x, typeName){
+	dx <- dim(x)
+	
+	out<-data.frame()
+	lastType<-NA
+	counter<-0
+	for(i in 1:nrow(x)){
 
-lastMeal<-NA
-counter<-0
-for(i in 1:length(titles)){
-	ti <- titles[i] 
+		if(grepl(x[i,1],'TOTAL:')) next
 
-	if(! ti %in%c('Foods','Exercises')){
-		lastMeal <- as.Date(ti, format = '%B %d,%Y')
-		next
+		if(all(is.na(x[i,-1]))) {
+			lastType <- x[i,1]
+			next
+		}
+
+		#if not a meal, then it's a food item
+		counter <- counter + 1
+		for(j in 1:ncol(x)){
+			out[counter,names(x)[j]] <- x[i,j]
+		}
+		out[counter,typeName] <- lastType
 	}
 
-	#if not a date, then it's a table
-	counter <- counter + 1
-	lastMeal)
+	out
 }
+
+
+
+allFood<-data.frame()
+for(i in 1:length(tab)){
+	if(isExercise[i]) next
+	thisFoodDay <- processTable(tab[[i]], typeName='meal')
+	thisFoodDay$day <- dates[i]
+
+	allFood <- rbind(allFood,thisFoodDay)
+}
+colnames(allFood) <- tolower(colnames(allFood))
+allFood <- as.tbl(allFood)
+allFood[,-1]
+allFood[,1]
+
+allExercise<-data.frame()
+for(i in 1:length(tab)){
+	if(isFood[i]) next
+	thisExerciseDay <- processTable(tab[[i]], typeName='ExerciseType')
+	thisExerciseDay$day <- dates[i]
+
+	allExercise <- rbind(allExercise,thisExerciseDay)
+}
+colnames(allExercise) <- tolower(colnames(allExercise))
+allExercise <- as.tbl(allExercise)
+
+allExercise$calories <- -1 * abs(allExercise$calories)
+
+allExercise
 
 
 
