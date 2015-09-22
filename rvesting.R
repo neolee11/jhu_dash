9135124603161f9b8bb9c9e9fb1706adbb7dd096
Aaron Fisher <fisher@jhu.edu>
2015-09-21 21:02:06 -0400

    touching up code, adding roxygen documentation for package

--- rvesting.R
+++ rvesting.R
@@ -1,24 +1,69 @@
-library(rvest)
 
-username <- 'funchords'
-fromDate <- '2015-08-01'
-toDate='2015-08-04'
+
+#' @title Scrape data from my fitness pal
+#'
+#' @param username for person of interest
+#' @param fromDate date to start gathering R data. Should have class "Date"
+#' @param toDate verbose whether to print progress
+#' @export
+#' @import rvest
+#' @return A list containing all exercises and all food items eaten
+#' @aliases
+#' @examples \dontrun{
+#' username = 'funchords'
+#' fromDate = as.Date('2015-08-01','%Y-%m-%d')
+#' toDate =   as.Date('2015-08-10','%Y-%m-%d')
+#' 
+#' z<-scrappal(
+#' 		username = username,
+#' 		fromDate = fromDate,
+#' 		toDate = toDate
+#' 		)
+#'
+#' ### Show food labels separately, because they're large
+#' z$food[1:10,1]
+#' ### preview everything but the label
+#' z$food[,-1]
+#'
+#' z$exercise
+#'
+#'}
+scrappal <- function(username='bcaffo', fromDate, toDate, verbose=getOption('verbose')){
+
+
+
+############
+############
+############
+# Retrieve data
+
+if(verbose) message('retrieving data from my fitness pal...')
 
 session<-html_session(paste0('http://www.myfitnesspal.com/reports/printable_diary/',username))
 
 form_blank <- html_form(html(session))[[1]]
 form_filled <- set_values(form_blank,
-	from=fromDate,
-  	to=toDate)
-out<-submit_form(session,form_filled)
+	from = as.character(fromDate, format='%Y-%m-%d'),
+  	to =   as.character(toDate,   format='%Y-%m-%d'))
+form_output<-submit_form(session,form_filled)
 
-tab<-html_table(out,header=TRUE) #gets tables
-hout<-html(out) #gets in html form
-# xout<-xmlTreeParse(out) #in xml form
+if(verbose) message('reformatting data...')
 
+tab<-html_table(form_output,header=TRUE) #gets tables
 
-
+hOut<-html(form_output) #gets in html form
+titles<-html_nodes(hOut,'#date , thead .first') %>%
+	html_text()
 # print(object.size(tab),units='Mb')
+
+# xOut<-xmlTreeParse(form_output) #in xml form, not currently used.
+
+
+#!!?? Do we need to "close" the `session` object? What do you think Daniel?
+
+
+
+
 
 
 ##########
@@ -26,15 +71,13 @@ hout<-html(out) #gets in html form
 ##########
 # Get Dates & Table Labels
 
-lt<-length(tab)
-dates <- rep(as.Date('January 1, 0001',format='%B %d,%Y'),lt)
 isFood<- unlist(lapply(tab, function(x) colnames(x)[1]=='Foods'))
 isExercise<- unlist(lapply(tab, function(x) colnames(x)[1]=='Exercises'))
-isFood + isExercise
+# all((isFood + isExercise) == 1) #workcheck
 
-titles<-html_nodes(hout,'#date , thead .first') %>%
-	html_text()
 
+#Assign dates to all elements of `tab`
+dates <- rep(as.Date('January 1, 0001',format='%B %d,%Y'),length(tab))
 
 lastDate<-NA
 counter<-0
@@ -51,9 +94,9 @@ for(i in 1:length(titles)){
 	dates[counter] <- as.Date(lastDate)
 }
 
-data.frame(dates,isFood,isExercise)
+#In-console summary of the tables we've retrieved
+as.tbl(data.frame(dates,isFood,isExercise))
 
-names(tab)
 
 
 processTable <- function(x, typeName){
@@ -63,18 +106,32 @@ processTable <- function(x, typeName){
 	lastType<-NA
 	counter<-0
 	for(i in 1:nrow(x)){
+		
+		#skip totals columns, as these are redundant
+		if(	grepl(x[i,1], 'TOTAL:',  fixed=TRUE) |
+			grepl(x[i,1], 'TOTALS:', fixed=TRUE)) next 
 
-		if(grepl(x[i,1],'TOTAL:')) next
-
-		if(all(is.na(x[i,-1]))) {
+		#Keep track of the most recent category header, but don't log it as a food or exercise item
+		if(all(is.na(x[i,-1]))) { 
 			lastType <- x[i,1]
 			next
 		}
 
-		#if not a meal, then it's a food item
+		#if not a meal-type (or exercise-type), then it's a food item (or exercise activity),
+		#so add it to our output data.frame
 		counter <- counter + 1
 		for(j in 1:ncol(x)){
-			out[counter,names(x)[j]] <- x[i,j]
+			x_ij <- x[i,j]
+			if(j>1){
+				#for non-labels:
+				# remove 'mg' and 'g' units. Also remove commas (from large numbers like 1,100)
+				#!! Check !! can users enter items in something other than 'g' or 'mg'? Do they have a choice of units? If so, this could break our parser
+				x_ij <- gsub('mg', '', x_ij, fixed=TRUE)
+				x_ij <- gsub('g',  '', x_ij, fixed=TRUE)
+				x_ij <- gsub(',',  '', x_ij, fixed=TRUE)
+				x_ij <- as.numeric(x_ij)
+			}
+			out[counter,names(x)[j]] <- x_ij
 		}
 		out[counter,typeName] <- lastType
 	}
@@ -88,14 +145,21 @@ allFood<-data.frame()
 for(i in 1:length(tab)){
 	if(isExercise[i]) next
 	thisFoodDay <- processTable(tab[[i]], typeName='meal')
+
 	thisFoodDay$day <- dates[i]
 
 	allFood <- rbind(allFood,thisFoodDay)
+		#bind more efficiently?? (not urgent, already fast). Probably the issue is the within table parsing, not the the combining.
 }
+
 colnames(allFood) <- tolower(colnames(allFood))
+allFood$username <- username
+
 allFood <- as.tbl(allFood)
 allFood[,-1]
 allFood[,1]
+
+unlist(lapply(allFood, class))
 
 allExercise<-data.frame()
 for(i in 1:length(tab)){
@@ -106,12 +170,23 @@ for(i in 1:length(tab)){
 	allExercise <- rbind(allExercise,thisExerciseDay)
 }
 colnames(allExercise) <- tolower(colnames(allExercise))
-allExercise <- as.tbl(allExercise)
-
 allExercise$calories <- -1 * abs(allExercise$calories)
+allExercise <- as.tbl(allExercise)
 
 allExercise
 
+
+#How to identify common activities:
+#1) concatenate all labels
+#2) split into words
+#3) table words
+#4) use grep to find words in activity names
+#Problem: will also return words like "the", "mph", "pace", etc. Ignore this issue for now?
+
+
+return(list(food = allFood, exercise = allExercise))
+
+}
 
 
 
