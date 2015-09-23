3a2cc230a73d6bfb1c0cfab868f593714ef56e8c
Aaron Fisher <fisher@jhu.edu>
2015-09-23 11:04:31 -0400

    adding help page

--- scrappal-package/inst/shiny_app/ui.R
+++ scrappal-package/inst/shiny_app/ui.R
@@ -2,7 +2,7 @@
 library(shiny)
 require(scrappal)
 
-
+# readHelpTabHTML<- paste0(readLines('help_tab.html'),collapse='') #will be converted to a the welcome (help) page 
 
 shinyUI(fluidPage(
   titlePanel("Scrappal"),
@@ -47,8 +47,44 @@ shinyUI(fluidPage(
     ),
     mainPanel(
       tabsetPanel(
+        # tabPanel("Instructions",
+          # HTML(paste(readHelpTabHTML,collapse=''))),
         tabPanel("Instructions",
-          "contents"),
+          br(),
+          p("The Scrappal Application provides an interface
+            for users of the 'My Fitness Pal' application
+            to get a summary report of their fitness
+            progress based on the inputs they provide to the
+            'My Fitness Pal' app. These inputs are scrapped
+            from the users' Fitness Diary."),
+          br(),
+          p("Once the input is retrieved, the Scrappal R package
+            organizes the data into 3 tables: food, exercise,
+            calories per day. It uses this information to display 3
+            plots for each user:"),
+          p(strong("* Weekly intake 'selected-nutrition' per day")),
+          p(strong("* Weekly trends")),
+          p(strong("* Weekly 'selected-nutrition' burned in exercise")),
+          br(),
+          p("The Scrappal app takes the following inputs:"),
+          p(strong("Username: "), ("registered username of 'My Fitness
+Pal' user")),
+          p(strong("Password: "), ("password for the 'My Fitness Pal'
+app for above user")),
+          em("Note: If password field is empty, the Scrappal app tries
+             to find if given username has a public dairy in 'My Fitness
+             Pal'. If not, an error is shown that dairy for user cannot
+             be accessed without the correct password."),
+          p(strong("Start date: "), ("start date from which to pull
+records from 'My Fitness Pal' for current analysis.")),
+          p(strong("End date: "), ("end date to pull records from 'My
+Fitness Pal' for current analysis.")),
+          br(),
+          p("The weekly intake tab takes the following additional inputs:")
+
+          ),
+
+
         tabPanel("Weekly Nutrition",
           plotOutput("dailyIntakePlot"),
           selectInput('mealBreak','Break nutrition down by meal',
